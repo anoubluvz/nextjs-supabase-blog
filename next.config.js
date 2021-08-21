3d072f39d82a91f4b69655ce790f5e13d34e3480
unknown <syntomy>
2021-08-21 20:50:32 +0200

    Upload project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    SUPABASE_URL: "https://bnedwynfcqirmlyzlzpn.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyOTEyNjI1MywiZXhwIjoxOTQ0NzAyMjUzfQ.BJqlTJ1B3iUZxjO_grkkhbrtIUre_Hq7zKTr5FfU5Fs",
+    SUPABASE_URL: "URL HERE",
+    SUPABASE_ANON_KEY: "ANON KEY HERE",
     PRODUCTION_URL: "http://localhost:3000"
   }
 }
